2cf94397e6cb0c61247916d613faf96733bb1a1e
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-28 18:21:53 -0400

    fix: authenticationKey function

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
-function authenticationKey(req, res) {
+function authenticationKey(req, res, next) {
   const api_key = req.header("x-api-key");
 
   if (api_key !== process.env.APIKEY) {
     res.status(403).json({ message: "You not allowed." });
   }
+  next();
 }
 
 module.exports = authenticationKey;
